2b8ed9e5669d30944b68e290ab1a8f2ba173f1db
Colm Prunty <colmpruntysignups+git@gmail.com>
2021-01-10 21:42:06 +0000

    delete this stuff

--- rectangles/target/debug/deps/rectangles.d
+++
@@ -1,5 +0,0 @@
-/Users/colmprunty/Documents/Code/Rust/projects/rectangles/target/debug/deps/rectangles: src/main.rs
-
-/Users/colmprunty/Documents/Code/Rust/projects/rectangles/target/debug/deps/rectangles.d: src/main.rs
-
-src/main.rs:
--- rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-/Users/colmprunty/Documents/Code/Rust/projects/rectangles/target/debug/rectangles: /Users/colmprunty/Documents/Code/Rust/projects/rectangles/src/main.rs
