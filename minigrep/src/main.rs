567f90274101b6bbc39e39c2de6d67a5c0c7c903
Colm Prunty <colmpruntysignups+git@gmail.com>
2021-03-05 21:29:35 +0000

    Start minigrep project

---
+++ minigrep/src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something is wrong");
+
+    println!("With text:\n{}", contents);
+}
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
