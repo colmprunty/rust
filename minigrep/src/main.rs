840af276185aad41fc4cde68ca9326f148bc52cd
Colm Prunty <colmpruntysignups+git@gmail.com>
2021-03-07 21:28:07 +0000

    small refactor

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,20 +3,24 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something is wrong");
+    let contents = fs::read_to_string(config.filename).expect("Something is wrong");
 
     println!("With text:\n{}", contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    (query, filename)
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
