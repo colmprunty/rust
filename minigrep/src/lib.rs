33e455e18bd6298b307cd9beb3ec5bb4cc7150d9
Colm Prunty <colmpruntysignups+git@gmail.com>
2021-06-09 21:41:35 +0100

    Starting tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -24,3 +24,23 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    vec![]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
